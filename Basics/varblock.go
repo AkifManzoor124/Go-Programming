66f84dbbb00fda02095a80f7abd68b3c72e22d7a
Akif Manzoor <akifmanzoor124@gmail.com>
2020-06-11 22:12:10 -0400

    added examples for zero values when variables are not initialized to a value

--- Basics/varblock.go
+++ Basics/varblock.go
@@ -6,14 +6,22 @@ import(
 )
 
 //we can define variables in a block just like imports
+//this eliminates the need to put var infront of each and every varialbe definition
 var(
 	foo, bar = 1, math.Pi
 	x,y bool = true, false
 	i,j int = 2, 1
+
+	//zero values example
+	jk int
+	balalala bool
+	s string
 )
 
 func main(){
 	fmt.Printf("Type: %T Value %v\n", foo,foo)
 	fmt.Printf("Type: %T Value %v\n", x,x)
 	fmt.Printf("Type: %T Value %v\n", i,i)
+
+	fmt.Printf("Can't touch this %v %v %v",jk,balalala,s)
 }
