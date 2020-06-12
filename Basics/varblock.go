5af818e76f4b511d86ae66d6c6dd85f4f5e63b5f
Akif Manzoor <akifmanzoor124@gmail.com>
2020-06-11 22:18:21 -0400

    Added examples for const variable definition

--- Basics/varblock.go
+++ Basics/varblock.go
@@ -18,10 +18,20 @@ var(
 	s string
 )
 
+//const variables cannot be changed once initialized to a value
+const Pi = 3.14
+const(
+	firstName = "Akif"
+	lastName = "Manzoor"
+)
+
+
 func main(){
 	fmt.Printf("Type: %T Value %v\n", foo,foo)
 	fmt.Printf("Type: %T Value %v\n", x,x)
 	fmt.Printf("Type: %T Value %v\n", i,i)
 
-	fmt.Printf("Can't touch this %v %v %v",jk,balalala,s)
+	fmt.Printf("Can't touch this %v %v %v\n",jk,balalala,s)
+
+	fmt.Printf("%v %v %v",firstName, lastName, Pi)
 }
