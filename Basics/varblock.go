3545c36f402e9050d4400608d4803186e20cdc91
Akif Manzoor <akifmanzoor124@gmail.com>
2020-06-11 22:02:58 -0400

    fixed up syntax errors

--- Basics/varblock.go
+++ Basics/varblock.go
@@ -6,14 +6,14 @@ import(
 )
 
 //we can define variables in a block just like imports
-var{
+var(
 	foo, bar = 1, math.Pi
 	x,y bool = true, false
 	i,j int = 2, 1
-}
+)
 
 func main(){
-	fmt.Printf("Type: %T Value %v\n", foo,bar)
-	fmt.Printf("Type: %T Value %v\n", x,y)
-	fmt.Printf("Type: %T Value %v\n", i,j)
+	fmt.Printf("Type: %T Value %v\n", foo,foo)
+	fmt.Printf("Type: %T Value %v\n", x,x)
+	fmt.Printf("Type: %T Value %v\n", i,i)
 }
