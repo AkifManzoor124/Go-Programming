bc4b736a23ee595f56709f9b5f7bf3dfd90c5de7
Akif Manzoor <akifmanzoor124@gmail.com>
2020-06-12 20:40:59 -0400

    Created while loop example

--- Flow Control/forloop.go
+++ Flow Control/forloop.go
@@ -7,4 +7,12 @@ func main(){
 	for i := 0; i < 10; i++ {
 		fmt.Println(i)	
 	}
+
+	//Go uses for as the for loop and as the while loop
+	//We need to increase j or else, we will only print 0s
+	var j int
+	for j < 1000 {
+		j = j + 1 
+		fmt.Println(j)
+	}
 }
