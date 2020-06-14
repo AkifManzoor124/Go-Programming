5dbc52750320881fc78a380d65a1ec40bdf68c45
Akif Manzoor <akifmanzoor124@gmail.com>
2020-06-14 18:05:09 -0400

    showing how defer statements are put into a stack

--- Flow Control/deferexample.go
+++ Flow Control/deferexample.go
@@ -2,8 +2,15 @@ package main
 
 import "fmt"
 
+//defer statements are put onto a stack and come out of the stack with the first in last out methodology
 func main() {
 	defer fmt.Println("You ain't shit")
 
 	fmt.Println("Get me a sandwich")
+
+	//1 will go in the stack first and will be printed last
+	//9 will print first
+	for i := 0; i < 10; i++ {
+		defer fmt.Println(i)
+	}
 }
